5498f997c14626ea0a4346d71193fa767ec9d90e
Mohamed Sharif <mohame@gmail.com>
2015-11-06 12:02:29 -0600

    first commit

---
+++ javascripts/main.js
@@ -0,0 +1,40 @@
+requirejs.config({
+  baseUrl: './javascripts',
+  paths: {
+    'jquery': '../lib/bower_components/jquery/dist/jquery.min',
+    'lodash': '../lib/bower_components/lodash/lodash.min',
+    'hbs': '../lib/bower_components/require-handlebars-plugin/hbs',
+    'bootstrap': '../lib/bower_components/bootstrap/dist/js/bootstrap.min',
+    'q': '../lib/bower_components/q/q',
+    'firebase' : '../lib/bower_components/firebase/firebase'
+  },
+  shim: {
+    'bootstrap': ['jquery'],
+    'firebase': {exports: 'Firebase'}
+  }
+});
+
+requirejs(
+  ["jquery", "lodash", "hbs", "bootstrap", "q"],
+  function($, _, Handlebars, bootstrap, q) {
+
+ 
+  // alert ("mohamed");
+  
+    // filter back to all
+
+    
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
